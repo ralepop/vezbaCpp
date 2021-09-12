27314a86f2faa5839658284bd6c58b8025a70daf
ralepop <rale.popovic04@gmail.com>
2021-09-12 20:28:14 +0200

    zbir

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 int main(){
-
     
+    int zbir = 0;
+    int brojevi[] = {3, 5, 6, 7, 1, 2};
+    for (int i = 0; i < *(&brojevi + 1) - brojevi; i++)
+    {
+        zbir += brojevi[i];
+    }
+    cout << zbir;
+    
+
     
     return 0;
 }
