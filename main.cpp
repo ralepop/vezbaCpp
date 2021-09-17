5708a576b338428df4830c28447dcf69538dbb8e
ralepop <rale.popovic04@gmail.com>
2021-09-17 20:44:46 +0200

    trobojka 2

--- main.cpp
+++ main.cpp
@@ -3,23 +3,7 @@ using namespace std;
 
 int main(){
     
-    long long k;
-    cin >> k;
-
-    long long pocetak = 1;
-    long long brojElemenata = 1;
-
-    for (int i = 1; i < k; i++){
-        pocetak += brojElemenata;
-        brojElemenata += 2;
-    }
-
-    long long zbirRedaTrougla = 0;
-    for (long long i = pocetak; i < pocetak + brojElemenata; i++){
-        zbirRedaTrougla += i;
-    }
     
-    cout << zbirRedaTrougla << endl;
    
 
     return 0;
