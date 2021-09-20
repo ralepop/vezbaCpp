70816043ac26e949fc22c7eac33955582967d8c5
ralepop <rale.popovic04@gmail.com>
2021-09-21 01:19:43 +0200

    zavrsena trobojka

--- main.cpp
+++ main.cpp
@@ -8,28 +8,20 @@
 
 using namespace std;
 
-// funkcija ucitava elemente u vektor
 vector<int> unosNiza(){
     int n;
     cin >> n;
     vector<int> a(n);
-    for (int i = 0; i < n; i++){
-        cin >> a[i];
-    }
+    for (int i = 0; i < n; i++){ cin >> a[i]; }
     return a;
 }
 
-// funkcija organizuje elemente vektora tako da se prvo nalaze 
-// elementi za koje vazi da su iz intervala (-Inf, A), nakon toga 
-// dolaze elementi iz intervala [A, B], i nakon toga elementi 
-// iz intervala (B, Inf)
 
 void podelaNiza(vector<int>& niz, int A, int B){
     int l = 0, i = 0, d = niz.size();
-
     while(i < d){
         if(niz[i] < A){
-            swap(niz[i++], niz[l++]);
+            swap(niz[i++], niz[l--]);
         }else if(niz[i] <= B){
             i++;
         }else{
@@ -40,6 +32,7 @@ void podelaNiza(vector<int>& niz, int A, int B){
 
 void ispisNiza(const vector<int>& a, int A, int B){
     int i = 0;
+
     while(i < a.size() && a[i] < A){
         cout << a[i++] << " ";
     }
@@ -54,7 +47,6 @@ void ispisNiza(const vector<int>& a, int A, int B){
         cout << a[i++] << " ";
     }
     cout << endl;
-    
 }
 
 int main(){
