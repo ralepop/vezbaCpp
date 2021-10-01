77c8b3520a2e5f5dfda43f8dc1a2054e98740419
ralepop <rale.popovic04@gmail.com>
2021-10-01 21:43:51 +0200

    aritmeticki_kvadrat

---
+++ aritmeticki_kvadrat.cpp
@@ -0,0 +1,20 @@
+long n, i, zbirH = 0, zbirV = 0;
+    cin >> n >> i;
+    
+
+    // za horizontalu
+    long pocetakBrojanjaH = i * n;
+    long krajBrojanjaH = pocetakBrojanjaH + (n - 1); 
+    for(long a = pocetakBrojanjaH; a <= krajBrojanjaH; a++){
+        zbirH += a;
+    }
+
+    // za vertikalu
+    long pocetakBrojanjaV = i;
+    long krajBrojanjaV = (pow(n, 2) - 1) - (n - i - 1);
+    for(long a = pocetakBrojanjaV; a <= krajBrojanjaV; a += n){
+        zbirV += a;
+
+    }
+
+    cout << zbirH << endl << zbirV;
